44989017a37f3a11714ef14fe6a6ef7cc8c977fc
Tao Ji <jitaoivan@outlook.com>
2018-12-05 11:41:13 +0800

    Discovery decoupled from node table

---
+++ libgfrp2p/include/discovery.h
@@ -0,0 +1,63 @@
+#include <string>
+#include <memory>
+
+#include "node_table.h"
+
+// UDP packet format
+class Packet {
+private:
+    // Can be extended with hash, signature, etc.
+    std::string payload;
+
+public:
+    // Assemble to UDP datagram
+    virtual std::string pack() const;
+    // Dissemble from UDP datagram
+    virtual void unpack(std::string datagram);
+
+    // Packet design-specific functions
+    std::string get_payload() const;
+
+    void set_payload(std::string new_payload);
+};
+
+
+class Discovery: private Receiver {
+    // Let wire protocol control the table
+    friend class AsyncUDPServer;
+private:
+
+    // UDP server for discovery
+    std::unique_ptr<AsyncUDPServer> udp_server;
+
+    // Node storage
+    std::shared_ptr<NodeTable> node_table;
+
+    // Implement udp receive call back
+    virtual void receive(const std::string& ip, unsigned short port, const std::string& data) override;
+    // Encapsulating low-level stuff
+    void send(const std::string& ip, unsigned short port, std::string payload);
+
+    /* Wire protocol calls */
+    void send_ping(const std::string& id, const std::string& ip, unsigned short port);
+    void send_pong(const std::string& ip, unsigned short port);
+    
+    void receive_ping(const std::string& ip, unsigned short port);
+    void receive_pong(const std::string& id, const std::string& ip, unsigned short port);
+
+    // discovery loop
+    void discovery();
+
+public:
+    static const char CMD_PING = 0x01;
+    static const char CMD_PONG = 0x02;
+
+    // A port and the storage for discovery must be given
+    Discovery(const std::shared_ptr<NodeTable>& node_table, unsigned short port);
+    
+    // Join the network and run the maintenance loop
+    void start();
+    // Leave the network
+    void stop();
+
+}
---
+++ libgfrp2p/src/discovery.cpp
@@ -0,0 +1,83 @@
+#include "discovery.h"
+
+std::string Packet::pack() const {
+    return this->payload;
+}
+// No need to disassemble in this particular case
+void Packet::unpack(std::string datagram) {
+    this->data = datagram;
+}
+
+std::string Packet::get_payload() const {
+    return this->payload;
+}
+
+void Packet::set_payload(std::string new_payload) {
+    this->payload = new_payload;
+}
+
+/* Constructor */
+Discovery::Discovery(const std::shared_ptr<NodeTable>& node_table, unsigned short port):
+    udp_server(new AsyncUDPServer(static_cast<Receiver*>(this), port)), node_table(node_table) { }
+
+/* Private functions */
+void Discovery::receive(const std::string& ip, unsigned short port, const std::string& datagram) override {
+    // Unpack the UDP datagram
+    std::unique_ptr<Packet> packet(new Packet());
+    packet->unpack(datagram);
+    std::string unpacked_data = packet->get_payload();
+
+    // Verify signature, etc.
+
+    // Disassemble data
+    std::string id = data.substr(0, 8);
+    char cmd = data.at(8);
+    std::string cmd_field = data.substr(9);
+
+    // Call the corresponding Node Table logic
+    if (cmd & CMD_PING) {
+        receive_ping(ip, port);
+    } else if (cmd & CMD_PONG) {
+        receive_pong(id, ip, port);
+    }
+
+}
+
+void Discovery::send(const std::string& ip, unsigned short port, std::string payload) {
+    Packet packet;
+    packet.set_data(payload);
+    this->udp_server->send(ip, port, packet.pack());
+}
+
+void Discovery::send_ping(const std::string& id, const std::string& ip, unsigned short port) {
+    std::string payload = self->id + CMD_PING;
+    this->send(ip, port, payload)
+    if (this->node_table->has_node(id)) {
+        this->node_table->get_node(id)->set_last_ping_now();
+    }
+}
+
+void Discovery::send_pong(const std::string& ip, unsigned short port) {
+    std::string payload = self->id + CMD_PONG;
+    this->send(ip, port, payload)
+}
+
+void Discovery::receive_ping(const std::string& ip, unsigned short port) {
+    send_pong(ip, port);
+}
+
+void Discovery::receive_pong(const std::string& id, const std::string& ip, unsigned short port) {
+    if (this->node_table->has_node(id)) {
+        this->node_table->get_node(id)->set_last_pong_now();
+    }
+}
+
+
+
+void Discovery::start() {
+    this->udp_server.run();
+}
+
+void Discovery::stop() {
+    // Disconnect to be implemented
+}
