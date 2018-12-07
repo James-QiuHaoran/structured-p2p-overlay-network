d0c9a94d3693aa98294fd35287794be702cac3ed
Tao Ji <jitaoivan@outlook.com>
2018-12-07 15:50:54 +0800

    Node table implemented

--- libgfrp2p/src/node_table.cpp
+++ libgfrp2p/src/node_table.cpp
@@ -1,37 +1,116 @@
 #include "node_table.h"
 
-std::string Packet::pack() const {
-    return this->data;
+/* Private functions */
+std::shared_ptr<Node> NodeTable::get_node(const std::string& id) {
+    for (const auto& r : table) {
+        auto contact_nodes_iter = r.contact_nodes.find(id),
+             predecessors_iter = r.predecessors.find(id),
+             successors_iter = r.successors.find(id);
+
+        if (contact_nodes_iter != r.end()) 
+            return *contact_nodes_iter;
+        if (contact_nodes_iter != r.end()) 
+            return *predecessors_iter;
+        if (contact_nodes_iter != r.end()) 
+            return *successors_iter;
+    }
+    return std::shared_ptr<Node>();
 }
 
-// No need to disassemble in this particular case
-void Packet::unpack(std::string datagram) {
-    this->data = datagram;
+std::shared_ptr<Node> NodeTable::copy_node(const std::shared_ptr<Node>& node) {
+    return std::make_shared<Node>(*node);
 }
 
-void std::string Packet::get_data() const {
-    return this->data
+/* Public functions */
+NodeTable::NodeTable(const std::string& self_id):
+    self_id(self_id), self_level(0) { }
+
+std::string NodeTable::get_self_id() const {
+    return this->self_id;
 }
 
-
-// Initialzie the udp server, but not run receiving
-WireProtocol::WireProtocol(unsigned short port):
-    udp_server(static_cast<Receiver*>(this), port) { }
-
-void WireProtocol::run() {
-    udp_server.run();
+unsigned long NodeTable::get_self_level() {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    return this->self_level;
 }
 
-void WireProtocol::receive(const std::string& ip, unsigned short port, const std::string& data) override {
-    // Unpack the UDP datagram
-    std::unique_ptr<Packet> packet(new Packet());
-    packet->unpack(data);
-    std::string unpacked_data = packet->get_data();
+bool NodeTable::has_node(const std::string& id) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    for (const auto& r : table) {
+        if (r.contact_nodes.find(id) != r.end() || 
+            r.predecessors.find(id) != r.end() || 
+            r.successors.find(id) != r.end())
+        
+            return true;
+    }
+    return false;
+}
 
-    // Verify signature, etc.
+std::shared_ptr<Node> NodeTable::get_node_copy(const std::string& id) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    for (const auto& r : table) {
+        auto contact_nodes_iter = r.contact_nodes.find(id),
+             predecessors_iter = r.predecessors.find(id),
+             successors_iter = r.successors.find(id);
 
-    // Get corresponding node, create one if not exist
+        if (contact_nodes_iter != r.end()) 
+            return copy_node(*contact_nodes_iter);
+        if (contact_nodes_iter != r.end()) 
+            return copy_node(*predecessors_iter);
+        if (contact_nodes_iter != r.end()) 
+            return copy_node(*successors_iter);
+    }
+    return std::shared_ptr<Node>();
+}
 
-    // TODO: get command id and call corresponding function
+void NodeTable::set_node_last_ping_now(const std::string& id) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    auto node = this->get_node(id);
+    if (!node) return;
+    node->set_last_ping_now();
+}
 
+void NodeTable::set_node_last_pong_now(const std::string& id) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    auto node = this->get_node(id);
+    if (!node) return;
+    node->set_last_pong_now();
+}
+
+bool NodeTable::is_contact_node(unsigned long level) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    return level < this->self_level;
+}
+
+std::unordered_set<std::shared_ptr<Node>> NodeTable::get_contact_nodes(unsigned long level) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    std::unordered_set<std::shared_ptr<Node>> result;
+    if (level > this->self_level) return result;
+
+    for (const auto& kv: this->table.at(level).contact_nodes) {
+        result.insert(this->copy_node(kv.second));
+    }  
+    return result;
+}
+
+std::unordered_set<std::shared_ptr<Node>> NodeTable::get_successors(unsigned long level) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    std::unordered_set<std::shared_ptr<Node>> result;
+    if (level > this->self_level) return result;
+
+    for (const auto& kv: this->table.at(level).successors) {
+        result.insert(this->copy_node(kv.second));
+    }  
+    return result;
+}
+
+std::unordered_set<std::shared_ptr<Node>> NodeTable::get_predecessors(unsigned long level) {
+    std::lock_guard<std::mutex> lock(this->mlock);
+    std::unordered_set<std::shared_ptr<Node>> result;
+    if (level > this->self_level) return result;
+
+    for (const auto& kv: this->table.at(level).predecessors) {
+        result.insert(this->copy_node(kv.second));
+    }  
+    return result;
 }
