c63688b5a6bbb109bf2d305bf7bde1ed8ae93ff7
Tao Ji <jitaoivan@outlook.com>
2018-12-07 13:43:08 +0800

    Node class modified

--- libgfrp2p/include/node.h
+++ libgfrp2p/include/node.h
@@ -1,20 +1,24 @@
 #include <string>
+#include <chrono>
 
 /* Model of nodes
- * For data storage only
- * Mutex lock might be neede */
+ * For data storage only */
 class Node {
 private:
     // P2P network info
     std::string id;
-    unsigned long level;
-    // using cnode = contact node
-    bool is_current_ring_cnode;
-    bool is_sub_ring_cnode;
-    
+    // unsigned long level;
+
     // Low-level info
     std::string ip;
     unsigned short port;
+
+    // using cnode = contact node
+    // bool is_current_ring_cnode;
+    // bool is_sub_ring_cnode;
+    std::chrono::time_point<std::chrono::system_clock> last_ping;
+    std::chrono::time_point<std::chrono::system_clock> last_pong;
+    
 
 public:
     // Constructor
@@ -22,18 +26,24 @@ public:
 
     // Getters
     std::string get_id() const;
-    unsigned long get_level() const;
-    bool get_is_current_ring_cnode() const;
-    bool get_is_sub_ring_cnode() const;
+    // unsigned long get_level() const;
+    // bool get_is_current_ring_cnode() const;
+    // bool get_is_sub_ring_cnode() const;
 
     std::string get_ip() const;
     unsigned int get_port() const;
 
-    // Setters
-    void set_level(unsigned long new_level);
-    void set_is_current_ring_cnode(bool new_value);
-    void set_is_sub_ring_cnode(bool new_value);
+    std::chrono::time_point<std::chrono::system_clock> get_last_ping() const;
+    std::chrono::time_point<std::chrono::system_clock> get_last_pong() const;
+    
 
+    // Setters
+    // void set_level(unsigned long new_level);
+    // void set_is_current_ring_cnode(bool new_value);
+    // void set_is_sub_ring_cnode(bool new_value);
     void set_ip(const std::string& new_ip);
     void set_port(unsigned int new_port);
+    
+    void set_last_pong_now();
+    void set_last_ping_now();
 };
