993e7eec7a8625a0752086b53aef00d0dad06cbc
James-QiuHaoran <jamesqiu@hku.hk>
2018-12-20 00:55:40 +0800

    add dist level boostrap

--- libgfrp2p/src/app.cpp
+++ libgfrp2p/src/app.cpp
@@ -32,39 +32,103 @@ void BaseApp::form_structure(int num_nodes_in_dist, int num_cnodes_in_dist,
         int num_nodes_in_continent, unsigned short starting_port_number) {
     // form network topology based ID
     std::string id_in_dist = this->node.get_id().substr(ID_SINGLE_START, ID_SINGLE_START+ID_SINGLE_LEN);
-    std::stringstream str_stream(id_in_dist);
-    std::stringstream ss;
+    std::string dist_id = this->node.get_id().substr(ID_DISTRICT_START, ID_DISTRICT_START+ID_DISTRICT_LEN);
+    std::string city_id = this->node.get_id().substr(ID_CITY_START, ID_CITY_START+ID_CITY_LEN);
+    std::string state_id = this->node.get_id().substr(ID_STATE_START, ID_STATE_START+ID_STATE_LEN);
+    std::string country_id = this->node.get_id().substr(ID_COUNTRY_START, ID_COUNTRY_START+ID_COUNTRY_LEN);
+    std::string continent_id = this->node.get_id().substr(ID_CONTINENT_START, ID_CONTINENT_START+ID_CONTINENT_LEN);
 
+    std::stringstream ss_node(id_in_dist);
     int node_id_in_dist = 0;
-    str_stream >> node_id_in_dist;
+    ss_node >> node_id_in_dist;
+
+    std::stringstream ss_dist(dist_id);
+    int dist_id_int = 0;
+    ss_dist >> dist_id_int;
+    int num_nodes_in_one_dist = num_nodes_in_dist;
+
+    int num_dists_in_city = num_nodes_in_city/num_cnodes_in_dist;
+    std::stringstream ss_city(city_id);
+    int city_id_int = 0;
+    ss_city >> city_id_int;
+    int num_nodes_in_one_city = num_nodes_in_one_dist * num_dists_in_city;
+
+    int num_cities_in_state = num_nodes_in_state/num_cnodes_in_dist;
+    std::stringstream ss_state(state_id);
+    int state_id_int = 0;
+    ss_state >> state_id_int;
+    int num_nodes_in_one_state = num_nodes_in_one_city * num_cities_in_state;
+
+    int num_states_in_country = num_nodes_in_country/num_cnodes_in_state;
+    std::stringstream ss_country(country_id);
+    int country_id_int = 0;
+    ss_country >> country_id_int;
+    int num_nodes_in_one_country = num_nodes_in_one_state * num_states_in_country;
+
+    int num_countries_in_continent = num_nodes_in_continent/num_cnodes_in_country;
+    std::stringstream ss_continent(continent_id);
+    int continent_id_int = 0;
+    ss_continent >> continent_id_int;
+    int num_nodes_in_one_continent = num_nodes_in_one_country * num_countries_in_continent;
 
     std::vector<Ring> tables;
-
-    // normal nodes - peer level
-    Ring table_peer;
-    table_peer.ring_level = 0;
     std::unordered_map<std::string, std::shared_ptr<Node>> contact_nodes;  // contact nodes of the ring
     std::shared_ptr<Node> predecessor;                                     // successor within the ring
     std::shared_ptr<Node> successor;                                       // predecessor within the ring
     std::unordered_map<std::string, std::shared_ptr<Node>> peer_set;       // peers in an unordered map
     std::vector<std::shared_ptr<Node>> peer_list;                          // used for broadcast within ring
 
+    // normal nodes - peer level
+    Ring table_peer;
+    table_peer.ring_level = 0;
+
     // add other peers
+    std::stringstream ss;
+    int starting_port_number_in_dist = starting_port_number + 
+                                       num_nodes_in_one_continent * continent_id_int +
+                                       num_nodes_in_one_country * country_id_int +
+                                       num_nodes_in_one_state * state_id_int +
+                                       num_nodes_in_one_city * city_id_int +
+                                       num_nodes_in_one_dist * dist_id_int;
+
     for (int i = 0; i < num_nodes_in_dist; i++) {
         ss << std::setw(9) << std::setfill('0') << i;
         std::string peer_id_in_dist = ss.str();
         std::string node_id = this->node.get_id().substr(0, ID_SINGLE_START) + peer_id_in_dist;
-        unsigned short port = starting_port_number;
+        unsigned short port = starting_port_number_in_dist + i;
         Node node(node_id, "127.0.0.1", port);
 
-        if (node_id_in_dist < num_cnodes_in_dist) {
-
+        // insert into contact node list
+        if (i < num_cnodes_in_dist) {
+            contact_nodes.insert({node_id, std::make_shared<Node>(node)});
         }
+
+        // check predecessor or successor
+        if (node_id_in_dist == 0 && i == num_nodes_in_dist - 1) {
+            predecessor = std::make_shared<Node>(node);
+        } else if (node_id_in_dist == num_nodes_in_dist - 1 && i == 0) {
+            successor = std::make_shared<Node>(node);
+        } else if (i == node_id_in_dist + 1) {
+            successor = std::make_shared<Node>(node);
+        } else if (i == node_id_in_dist - 1) {
+            predecessor = std::make_shared<Node>(node);
+        }
+
+        // insert into peer list
+        peer_set.insert({node_id, std::make_shared<Node>(node)});
+        peer_list.push_back(std::make_shared<Node>(node));
     }
+
+    table_peer.contact_nodes = contact_nodes;
+    table_peer.predecessor = predecessor;
+    table_peer.successor = successor;
+    table_peer.peer_set = peer_set;
+    table_peer.peer_list = peer_list;
 
     tables.push_back(table_peer);
     
     // contact nodes - dist level
+    // [TODO] reset stringstream and ring variables
     if (node_id_in_dist < num_cnodes_in_dist) {
         // should be the contact node of the dist level ring
         Ring table_dist;
@@ -79,7 +143,6 @@ void BaseApp::form_structure(int num_nodes_in_dist, int num_cnodes_in_dist,
     }
 
     // contact nodes - city level
-    int num_dists_in_city = num_nodes_in_city/num_cnodes_in_dist;
     if (node_id_in_dist < num_cnodes_in_city/num_dists_in_city) {
         // should be the contact node of the city level ring
         Ring table_city;
@@ -94,7 +157,6 @@ void BaseApp::form_structure(int num_nodes_in_dist, int num_cnodes_in_dist,
     }
 
     // contact nodes - state level
-    int num_cities_in_state = num_nodes_in_state/num_cnodes_in_dist;
     if (node_id_in_dist < num_cnodes_in_state/num_cities_in_state) {
         // should be the contact node of the state level ring
         Ring table_state;
