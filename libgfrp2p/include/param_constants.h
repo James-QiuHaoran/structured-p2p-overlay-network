3a5515d4582fda2fcb8a2976b01c1ee8e17ab525
James-QiuHaoran <jamesqiu@hku.hk>
2018-12-19 16:35:23 +0800

    create bootstrap

--- libgfrp2p/include/param_constants.h
+++ libgfrp2p/include/param_constants.h
@@ -6,15 +6,19 @@
 #define HEARTBEAT_TIMEOUT  5
 #define NUM_CONTACT_NODES  3
 #define MSG_HASH_LENGTH   50
+
+// ID definition rules
 #define ID_CONTINENT_LEN   3
 #define ID_COUNTRY_LEN     4
 #define ID_STATE_LEN       5
-#define ID_CITY_LEN        5
-#define ID_SINGLE_LEN     10
+#define ID_CITY_LEN        6
+#define ID_DISTRICT_LEN    5
+#define ID_SINGLE_LEN     11
 #define ID_CONTINENT_START 0
 #define ID_COUNTRY_START   3
 #define ID_STATE_START     7
 #define ID_CITY_START     12
-#define ID_SINGLE_START   17
+#define ID_DISTRICT_START 18
+#define ID_SINGLE_START   23
 
 #endif
