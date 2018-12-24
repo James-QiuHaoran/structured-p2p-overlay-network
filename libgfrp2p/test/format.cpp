9ee61bd213e1899802120a2752d53ae06776d214
Tao Ji <jitaoivan@outlook.com>
2018-12-24 14:13:13 +0800

    Format test added

---
+++ libgfrp2p/test/format.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "format.h"
+
+
+int main() {
+    ConfigFormatter fmt1, fmt2;
+    fmt1.run_id = 19903;
+    fmt1.node_id = "00001000010000100001000010000110";
+
+    fmt1.num_nodes_in_dist = 1;
+    fmt1.num_cnodes_in_dist = 2;
+    fmt1.num_nodes_in_city = 3;
+    fmt1.num_cnodes_in_city = 4;
+    fmt1.num_nodes_in_state = 5;
+    fmt1.num_cnodes_in_state = 6;
+    fmt1.num_nodes_in_country = 7;
+    fmt1.num_cnodes_in_country = 8;
+    fmt1.num_nodes_in_continent = 9;
+
+    fmt1.num_table_entries = 2;
+    fmt1.table_entries["10001000010000100001000010000110"] = "192.168.100.100";
+    fmt1.table_entries["11001000010000100001000010000110"] = "192.168.100.101";
+
+    fmt2.from_string(fmt1.to_string());
+    
+    std::cout << fmt1.to_string() == fmt2.to_string() << std::endl;
+    std::cout << command_id + '0' << ' ' << run_id << ' ' << num_nodes_in_continent << ' ' << num_table_entries << std::endl;
+    
+    for (const auto & kv:table_entries){
+        std::cout << kv.first << " -> " << kv.second;
+    } 
+    return 0;
+}
