d026300e979acc8cba89772925eaab1c653ff800
Tao Ji <jitaoivan@outlook.com>
2019-01-07 21:19:16 +0800

    Correct the figure

--- experiments/eos_hgfr_micro_tps_on_input.py
+++ experiments/eos_hgfr_micro_tps_on_input.py
@@ -10,14 +10,14 @@ y_our = []
 for line in lines[1:]:
     fields = line.split('\t')
     x.append(float(fields[0]))
-    y_our.append(float(fields[1]))
-    y_eth.append(float(fields[2]))
+    y_eth.append(float(fields[1]))
+    y_our.append(float(fields[2]))
 
 fig = plt.figure(figsize=(5,4))
 
 ax = fig.add_subplot(111)
-plt.plot(x, y_eth, marker='s', markersize=6, linewidth=2, label='100Mbps')
-plt.plot(x, y_our, marker='s', markersize=6, linewidth=2, label='50Mbps')
+plt.plot(x, y_eth, marker='s', markersize=6, linewidth=2, label='EOS')
+plt.plot(x, y_our, marker='s', markersize=6, linewidth=2, label='HGFRR')
 ax.set_xticks(list(range(0,400, 100)))
 ax.set_xticklabels(list(range(0,400, 100)), fontsize=12, weight="bold")
 ax.set_yticks(list(range(0,250,50)))
